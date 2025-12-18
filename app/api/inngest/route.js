8a6b8e4921296b8cb8474abc2827887b060b772e
Ratax397 <ifalyfaly84@gmail.com>
2025-12-18 10:14:10 +0300

    first commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest.js";
+
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
